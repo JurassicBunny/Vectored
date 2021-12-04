83c257a5076e8a71f87f77dcfe579c2845c620cd
Matthew <matthew@Matthews-MBP.attlocal.net>
2021-12-04 01:01:13 -0500

    Inital Commit

---
+++ library.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Eigen/Dense>
+
+namespace Internal {
+
+class Vectored : public Eigen::Vector3d {
+public:
+    Vectored();
+    Vectored(double x, double y, double z);
+};
+
+} // Internal
+
+namespace Vectored {
+
+#define VECTORED(name) \
+class name : public Internal::Vectored { \
+public:                \
+    using Internal::Vectored::Vectored;\
+} \
+
+} // Vectored
+
+using namespace Vectored;
