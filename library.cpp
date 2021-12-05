ad2d0231c5a367d158ecb2d92e75b7b5eba9013f
Matthew <matthew@Matthews-MBP.attlocal.net>
2021-12-05 13:30:00 -0500

    add common use cases to namespace Vectored

--- library.cpp
+++ library.cpp
@@ -1,15 +1,16 @@
 #include "library.h"
 
- namespace Internal {
-
+namespace Internal {
 Vectored::Vectored()
     : Eigen::Vector3d() {}
 
 Vectored::Vectored(double x, double y, double z)
     : Eigen::Vector3d(x, y, z) {}
- }
+ } // Internal
 
+namespace Vectored {
 VECTORED(Acceleration);
 VECTORED(Force);
 VECTORED(Position);
 VECTORED(Velocity);
+} // Vectored
