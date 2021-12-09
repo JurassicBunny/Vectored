af23c941d0285aeb0d4f38f64f8f27b18d8ea2d7
Matthew <matthew@Matthews-MBP.attlocal.net>
2021-12-09 17:46:14 -0500

    add internal namespace to 'Vectored' namesapce

--- src/vectored.h
+++ src/vectored.h
@@ -5,48 +5,57 @@
 
 #include <Eigen/Dense>
 
+namespace Vectored {
+
 namespace Internal {
+
 class SuperVectored : public Eigen::Vector3d {
 public:
     SuperVectored()
-        : Eigen::Vector3d() {}
+            : Eigen::Vector3d() {}
 
     SuperVectored(double x, double y, double z)
-        : Eigen::Vector3d(x, y, z) {}
+            : Eigen::Vector3d(x, y, z) {}
 
     explicit SuperVectored(Eigen::Vector3d vector)
-        : Eigen::Vector3d(std::move(vector)) {}
+            : Eigen::Vector3d(std::move(vector)) {}
 
     Eigen::Vector3d as_vec() {
-        return Eigen::Vector3d{this -> x(),
-                               this -> y(),
-                               this -> z()};
+        return Eigen::Vector3d{this->x(),
+                               this->y(),
+                               this->z()};
     }
 
     template<typename T>
-        requires (std::is_base_of_v<SuperVectored, T>)
+        requires(std::is_base_of_v<SuperVectored, T>)
     explicit SuperVectored(T &vector) { T result(*this); }
 
     template<typename T>
-       requires (std::is_base_of_v<SuperVectored, T>)
-    T as() { T result(*this); return result;}
+        requires(std::is_base_of_v<SuperVectored, T>)
+    T as() {
+        T result(*this);
+        return result;
+    }
 };
 
-template <typename Class>
+template<typename Class>
 concept VectoredQuantity =
-     std::is_base_of<SuperVectored, Class>::value;
+    std::is_base_of<SuperVectored, Class>::value;
+
 } // Internal
 
+} // Vectored
+
 #define VECTORED(name) \
-class name : public Internal::SuperVectored { \
+class name : public Internal::SuperVectored {     \
 public:                \
     using Internal::SuperVectored::SuperVectored; \
                        \
-    template<Internal::VectoredQuantity T>                    \
+    template<Internal::VectoredQuantity T>        \
     name operator+(T &other) { this -> as_vec() += other.as_vec(); return {}; } \
                        \
-    template<Internal::VectoredQuantity T>                    \
-    name operator-(T &other) { this -> as_vec() -= other.as_vec(); return {}; }                       \
+    template<Internal::VectoredQuantity T>        \
+    name operator-(T &other) { this -> as_vec() -= other.as_vec(); return {}; } \
 }
 
 namespace Vectored {
