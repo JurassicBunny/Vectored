3f3e264bf0826bfc578c1d9efa324175cbc7cc1a
Matthew <matthew@Matthews-MBP.attlocal.net>
2021-12-13 13:55:29 -0500

    update

---
+++ test/testing.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Matthew on 12/13/21.
+//
+
+#include "../include/Vectored/vectored.h"
+#include <gtest/gtest.h>
+
+TEST(Constructor, Constructor)  {
+    using namespace Vectored;
+    Acceleration acceleration(1,2,3);
+    EXPECT_TRUE(acceleration.x() = 1);
+    EXPECT_TRUE(acceleration.y() = 2);
+    EXPECT_TRUE(acceleration.z() = 3);
+}
+
+TEST(Normal, Norm ) {
+    using namespace Vectored;
+    int answer = 1;
+    Acceleration acceleration(1, 2, 3);
+    auto result = acceleration.normalized().norm();
+    ASSERT_EQ(result, answer);
+}
+
+TEST(Operator, Addition) {
+    using namespace Vectored;
+    Acceleration acceleration(1, 2, 3);
+    Velocity velocity(2,5,6);
+    Acceleration result = acceleration + velocity;
+    Acceleration answer(3,7, 9);
+    EXPECT_EQ(result, answer);
+}
+
+TEST(Operator, Subtraction) {
+    using namespace Vectored;
+    Acceleration acceleration(1, 2, 3);
+    Velocity velocity(2,5,6);
+    Acceleration result = acceleration - velocity;
+    Acceleration answer(-1,-3, -3);
+    EXPECT_EQ(result, answer);
+}
